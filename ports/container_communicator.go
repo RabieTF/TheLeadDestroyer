2855e5451363dd74adccde9d6a446d2c645b7946
Rabie Toaba <rabie-fouzi.toaba@etu.univ-lehavre.fr>
2025-01-26 17:37:33 +0100

    correct container communicator port

--- ports/container_communicator.go
+++ ports/container_communicator.go
@@ -1,19 +1,14 @@
 package ports
 
+import (
+	"github.com/gorilla/websocket"
+)
+
 // ContainerCommunicator defines the interface for communicating with worker containers.
 type ContainerCommunicator interface {
-	// SendMessage Send sends a message to a specific container.
 	SendMessage(containerID string, message []byte) error
-
-	// ReceiveMessage Receive receives a message from a specific container.
 	ReceiveMessage(containerID string) error
-
-	// HandleDisconnect disconnects a specific container.
-	HandleDisconnect(containerID string) error
-
-	// AddConnection adds a new container connection.
-	AddConnection(containerID, url string) error
-
-	// ListConnections returns the IDs of all connected containers.
+	RemoveConnection(containerID string)
+	AddConnection(containerID string, conn *websocket.Conn)
 	ListConnections() []string
 }

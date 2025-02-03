389c55317aa011d0b520e94d7abef60346a489f5
Rabie Toaba <rabie-fouzi.toaba@etu.univ-lehavre.fr>
2025-02-03 12:32:17 +0100

    string split

--- application/handlers/solution_receiver.go
+++ application/handlers/solution_receiver.go
@@ -2,12 +2,14 @@ package handlers
 
 import (
 	"log"
+	"strings"
 	"www-apps.univ-lehavre.fr/forge/themd5destroyers/theleaddestroyer/adapters/websocket_adapter"
 )
 
 type SolutionReceiver struct {
 	containerWSAdapter *websocket_adapter.ContainerWebSocketAdapter
 	resultChannel      chan string
+	distributor        *TaskDistributor
 }
 
 func NewSolutionReceiver(containerWSAdapter *websocket_adapter.ContainerWebSocketAdapter, resultChannel chan string) *SolutionReceiver {
@@ -22,7 +24,9 @@ func (s *SolutionReceiver) Start() {
 	for message := range s.containerWSAdapter.SolutionChannel {
 		select {
 		case s.resultChannel <- message:
-			log.Printf("Forwarded result to resultChannel: %s\n", message)
+			id, msg := strings.Split(message, ":")[0], strings.Split(message, ":")[1]
+			s.distributor.markWorkerAvailable(id)
+			log.Printf("Forwarded result to resultChannel: %s\n", msg)
 		default:
 			log.Printf("ResultChannel is full. Dropped message: %s\n", message)
 		}
